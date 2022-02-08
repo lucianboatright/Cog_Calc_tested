32def0e6f5377bbc4b7b074e9b34b4814965ae76
luke boat <lucian.boatright@gmail.com>
2022-02-08 15:55:47 +0000

    front cog component passing all tests

--- src/components/Cogs/DriveTrain.tsx
+++ src/components/Cogs/DriveTrain.tsx
@@ -1,18 +1,33 @@
 import React, { useState } from 'react'
+import { FrontCog } from './FrontCogs'
 import { RearCog } from './RearCog'
 
 export const RearBasicCogs = () => <RearCog onRearShift={rearCog => rearCog} />
+export const FrontBasicCogs = () => <FrontCog onFrontShift={frontCog => frontCog} />
 
 export function DriveTrain() {
     const [rearCog, setRearCog] = useState(15)
+    const [frontCog, setFrontCog] = useState(50)
     return (
         <>
-            <p>Rear Cog: {rearCog}</p>
-            <RearCog 
-                onRearShift={rearCog => {
-                    setRearCog(rearCog)
-                }}
-            />
+            <div>
+                <div>
+                    <p>Rear Cog: {rearCog}</p>
+                    <p>Front Cog: {frontCog}</p>
+                </div>
+                <div>
+                    <RearCog 
+                        onRearShift={rearCog => {
+                            setRearCog(rearCog)
+                        }}
+                    /> 
+                    <FrontCog 
+                        onFrontShift={frontCog => {
+                            setFrontCog(frontCog)
+                        }}
+                    />
+                </div>
+            </div>
         </>
     )
 }
--- src/components/Cogs/FrontCogs.tsx
+++ src/components/Cogs/FrontCogs.tsx
@@ -10,16 +10,40 @@ export type FrontCogProps = {
 } & React.HtmlHTMLAttributes<HTMLDivElement>
 
 export function FrontCog({
-    min = 26,
+    min = 24,
     max = 64,
     frontIncroment = 1,
     frontDecrement = 1,
     onFrontShift
 }: FrontCogProps) {
 
+    const [frontCog, setFrontCog] = useState(50)
+
+    function handleShiftUp() {
+        if (frontCog < max) {
+            setFrontCog(frontCog + frontIncroment)
+            onFrontShift(frontCog + frontIncroment)
+        }
+    }
+
+    function handleShiftDown() {
+        if(frontCog > min) {
+            setFrontCog(frontCog + frontDecrement)
+            onFrontShift(frontCog + frontDecrement)
+        }
+    }
+
+    let handleInputShift = (event: ChangeEvent<HTMLInputElement>) => {
+        setFrontCog(event.currentTarget.valueAsNumber)
+        onFrontShift(event.currentTarget.valueAsNumber)
+    }
+
     return (
-        <div>
+        <div data-testid={'frontCog'}>
             <div>Front Cog</div>
+            <button onClick={handleShiftUp}>+</button>
+            <button onClick={handleShiftDown}>-</button>
+            <input type='number' min={min} max={max} value={frontCog} onChange={(e) => handleInputShift(e)}></input>
         </div>
     )
 }
