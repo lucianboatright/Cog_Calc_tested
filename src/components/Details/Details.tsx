59f3a32f3731248c0ae3a07aa80dcd88219d8dea
luke boat <lucian.boatright@gmail.com>
2022-02-22 14:39:09 +0000

    save state pre merge

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, FC } from 'react';
+import React, { useContext, FC, useState } from 'react';
 import { FrontCogContext } from '../Context/FrontCogContext';
 import { RearCogContext } from '../Context/RearCogContext';
 import { CrankContext } from '../Context/CrankContext';
@@ -8,28 +8,37 @@ const Detail = ({ tyreText, tyreTextValue, wheelSize, wheelSizeValue }: any) => 
     const {frontCog} = useContext(FrontCogContext)
     const {rearCog} = useContext(RearCogContext)
     const {crankRpm} = useContext(CrankContext)
-    const tyreValue = tyreTextValue
-    const wheelValue = wheelSizeValue
 
+    
+    const tyreTextCondition = tyreText ? tyreText : "Select Tyre Value"
+    const wheelTextCondition = wheelSize ? wheelSize : "Select Wheel Size"
 
-    const GearRatio = (frontCog / rearCog).toPrecision(3)
-    const TotalWheelDiameter = (wheelValue + (tyreValue * 2))
-    const WheelCircumferance = (TotalWheelDiameter * Math.PI)
-    const Speed = (WheelCircumferance * parseFloat(GearRatio) * crankRpm * 60)
+    const tyreValue = tyreTextValue ? tyreTextValue : 0
+    const wheelValue = wheelSizeValue ? wheelSizeValue : 0
+
+    const GearRatio = (Number(frontCog) / Number(rearCog)).toFixed(2)
+    const TotalWheelDiameter = (Number(wheelValue) + (Number(tyreValue) * 2))
+    const WheelCircumferance = ((TotalWheelDiameter * Math.PI) / 10)
+    const Speed = (WheelCircumferance * parseFloat(GearRatio) * crankRpm * 60) / 100000
+    const GearInches = ((Number(GearRatio) * WheelCircumferance) / 100)
 
     console.log("Gear Ratio", GearRatio)
+    console.log("Gear Inches", GearInches.toFixed(2))
     console.log("total wheel diameter", TotalWheelDiameter)
-    console.log("Wheel cuircumferance", WheelCircumferance)
+    console.log("Wheel cuircumferance", WheelCircumferance.toFixed(2))
     console.log("Speed", Speed)
 
     return (
         <div className='DatailsSection'>
             <div className='DetailsTitle' data-testid={'DetailsTitle'} >Details</div>
             <div className='Cogs_Display'><span data-testid="Details_Cogs_Rear">Rear Cog: {rearCog} - </span><span data-testid="Details_Cogs_Front">Front Cog: {frontCog}</span></div>
-            <div className='Gear_ratio' data-testid="Gear_ratio">Gear Ratios {GearRatio}</div>
+            <div className="Wheel_size_Details">Wheel Size: {wheelTextCondition}</div>
+            <div className="Tyre_size_Details">Tyre Size: {tyreTextCondition}</div>
+            <div className='Gear_ratio' data-testid="Gear_ratio">Wheel Cuircumferance: {WheelCircumferance.toFixed(2)} Meters</div>
+            <div className='Gear_ratio' data-testid="Gear_ratio">Gear Ratios: {GearRatio}</div>
+            <div className='Gear_ratio' data-testid="Gear_ratio">Gear Inches: {GearInches.toFixed(2)}</div>
             <div className='Cadance_rpm_Datails' data-testid="Cadance_rpm_Datails">Cadance: {crankRpm}rpm </div>
-            <div className="Wheel_size_Details">Wheel Size: {wheelSize ? wheelSize : "Select Wheel"}</div>
-            <div className="Tyre_size_Details">Tyre Size: {tyreText ? tyreText : "Select Tyre"}</div>
+            <div className='Cadance_rpm_Datails' data-testid="Cadance_rpm_Datails">Speed: {Speed.toFixed(2)} Km/h </div>
         </div>
     )
 }
